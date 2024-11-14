3a20a5db50147f3d62b76b2b793104dccd5e3fd5
sarahting101 <sarah30135@gmail.com>
2024-11-14 11:26:35 +0800

    finish argmax.s

--- src/argmax.s
+++ src/argmax.s
@@ -25,13 +25,32 @@ argmax:
     li t6, 1
     blt a1, t6, handle_error
 
-    lw t0, 0(a0)
+    lw t0, 0(a0)    #t0=max
 
-    li t1, 0
+    #li t1, 0        #t1=max_index
+    add t1, a1, x0
     li t2, 1
+    add t4, a1 , x0
 loop_start:
     # TODO: Add your own implementation
+    lw t3, 0(a0)
+    blt t3, t0, not_max
+    beq t3, t0, not_max
+    add t0, t3 , x0
+    add t1, a1 , x0
+    
+not_max:
+    addi a0, a0, 4
+    addi a1, a1, -1
+    bne a1, x0, loop_start
+    sub t1, t4, t1
+    add a0, t1 , x0
+    ret
 
 handle_error:
     li a0, 36
     j exit
+
+exit:
+    li a7, 10
+    ecall
